e4ffa422cfa2d972e36d63c2c5b9eb3f0a78105e
jhoandvid <rojasalmariojhoandvid@gmail.com>
2023-01-24 14:40:22 -0500

    add component in routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,8 +3,12 @@ import React from "react";
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Layout from "../container/Layout";
 import Login from "../container/Login";
+import Home from "../pages/Home";
+
+
 import RecoveryPassword from "../container/RecoveryPassword";
 import '../styles/global.css';
+import NotFound from "../pages/NotFount";
 
 
 
@@ -13,14 +17,14 @@ import '../styles/global.css';
 const App=()=>{
     return(
         <BrowserRouter>
-       <Switch>
-        <Layout>
-            <Route exact path="/home" component={Home}/>
-            <Route exact path="/login" component={Login}/>
-            <Route exact path="/recover-password" component={RecoveryPassword}/>
-            <Route component={NotFound}/>
-        </Layout>
-       </Switch>
+        <Switch>
+            <Layout>
+                <Route exact path="/home" component={Home}/>
+                <Route exact path="/login" component={Login}/>
+                <Route exact path="/recover-password" component={RecoveryPassword}/>
+                <Route path="/not-found" component={NotFound}/>
+            </Layout>
+        </Switch>
         </BrowserRouter>
     
      
