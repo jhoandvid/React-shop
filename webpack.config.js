e49e33230ee2f3026d9bfcd0b57fb0bd0244c9f6
jhoandvid <rojasalmariojhoandvid@gmail.com>
2023-01-27 19:05:41 -0500

    add alias in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -17,41 +17,55 @@ module.exports={
     },
     
     resolve:{
-        extensions:['.js', '.jsx']
+        extensions:['.js', '.jsx'],
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components/'),
+            '@containers': path.resolve(__dirname, 'src/containers/'),
+            '@pages': path.resolve(__dirname, 'src/pages/'),
+            '@styles': path.resolve(__dirname, 'src/styles'),
+            '@icons': path.resolve(__dirname, 'src/assets/icons/'),
+            '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+          }
     },
+    
 
 
     "mode": 'production',
 
     module:{
-        rules:[
-          {  
-            test:/\.(js|jsx)$/,
-            exclude:/node_modules/,
-            use:{
-                loader:'babel-loader'
-            }
-          },
-
-          {
-            test:/\.html$/,
-            use:{
-                loader:'html-loader'
-            }
-          },
-        
-          {
-            test:/\.(css|scss)$/,
-            use:['style-loader', 'css-loader', 'sass-loader']
-         },
-
-         /*  {
-            test:/\.css$/i,
-            use:[MiniCssExtractPlugin.loader, 'css-loader'], 
-          } */
-
-
-        ]
+         rules:[
+            {  
+              test:/\.(js|jsx)$/,
+              exclude:/node_modules/,
+              use:{
+                  loader:'babel-loader'
+              }
+            },
+  
+            {
+              test:/\.html$/,
+              use:{
+              
+                  loader:'html-loader'
+              }
+            },
+          
+            {
+              test:/\.(css|scss)$/,
+              use:['style-loader', 'css-loader', 'sass-loader']
+           },
+  
+           {
+              test:/\.(png|svg|jpg|gif)$/,
+              type:'asset'
+          }
+            /* {
+              test:/\.css$/i,
+              use:[MiniCssExtractPlugin.loader, 'css-loader']
+            }  */
+  
+  
+          ]
     },
 
 
